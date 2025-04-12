13e740d9cc276cc94df382c96d61140fa1b41b49
joaorocha-web <jvrochajf@eaportal.org>
2025-04-11 23:34:46 -0300

    .

--- seed.js
+++ seed.js
@@ -1,6 +1,9 @@
 function mostrarFotoCp() {
     let foto = document.getElementById('foto');
-
+    let nome = document.getElementById('nome');
+    let descricao = document.getElementById('sobre');
+    nome.innerHTML = "João Victor"
+    descricao.innerHTML ="<p>Copywriter com experiência em projetos nacionais e internacionais, incluindo trabalhos para uma musicista da Suíça. Atua na criação de textos estratégicos voltados para posicionamento de marca e conversão.</p><p>Especialista em adaptar a linguagem conforme o público e o canal, desenvolve copys para páginas de vendas, redes sociais e campanhas, unindo criatividade e foco em resultados.</p>"
     if (!foto) {
         console.error("Elemento 'foto' não encontrado!");
         return;
@@ -20,6 +23,10 @@ function mostrarFotoCp() {
 
 function mostrarFotoGt(){
     let foto = document.getElementById('foto');
+    let nome = document.getElementById('nome');
+    let descricao = document.getElementById('sobre');
+    nome.innerHTML = "Luis Felippe"
+    descricao.innerHTML = "<p>Gestor de tráfego com ampla atuação em campanhas para grandes e-commerces e empresas de diversos segmentos. Já gerenciou estratégias no Brasil e no exterior, otimizando resultados com foco em performance.</p><p>Com domínio de tráfego pago em múltiplas plataformas, entrega campanhas escaláveis, bem segmentadas e orientadas por dados, sempre com foco em conversão e retorno sobre investimento.</p>"
 
     if (!foto) {
         console.error("Elemento 'foto' não encontrado!");
@@ -40,6 +47,10 @@ function mostrarFotoGt(){
 
 function mostrarFotoMk(){
     let foto = document.getElementById('foto');
+    let nome = document.getElementById('nome');
+    let descricao = document.getElementById('sobre');
+    nome.innerHTML = "Pedro Araújo"
+    descricao.innerHTML = "<p>Especialista em vendas e estratégias de marketing com experiência em impulsionar resultados comerciais por meio de posicionamento eficaz e táticas inteligentes. Atuou em projetos de diferentes portes, sempre com foco em crescimento e conversão.</p><p>Com visão estratégica e domínio de funis de vendas, desenvolve ações integradas que alinham marketing e comercial, otimizando cada etapa da jornada do cliente.</p>"
 
     if (!foto) {
         console.error("Elemento 'foto' não encontrado!");
