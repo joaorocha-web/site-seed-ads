4f8d679fcef7c39d79601f5895c9ffc8e4e11bae
joaorocha-web <jvrochajf@eaportal.org>
2025-04-13 11:49:32 -0300

    .

--- script.js
+++
@@ -1,9 +0,0 @@
-function mostrarMenu(){
-    let menu = document.getElementsByTagName('menu')[0]
-    if (menu.style.display === 'none'){
-          menu.style.display= 'block'
-    } else {
-        menu.style.display = 'none'
-    }
-   
-}
